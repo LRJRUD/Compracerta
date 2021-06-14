039de0599d0b246f7e309d329f93b3d3db450be2
Dg-Alves <douglas.estefanio@gmail.com>
2021-06-13 23:16:40 -0300

    tradução
    O mestre da tradução voltou

--- View/Checkout.php
+++ View/Checkout.php
@@ -20,107 +20,73 @@
                         <div class="col-lg-8">
                             <div class="checkout-inner">
                                 <div class="billing-address">
-                                    <h2>Billing Address</h2>
+                                    <h2>Endereço de Cobrança</h2>
                                     <div class="row">
                                         <div class="col-md-6">
-                                            <label>First Name</label>
-                                            <input class="form-control" type="text" placeholder="First Name">
+                                            <label>Primeiro nome</label>
+                                            <input class="form-control" type="text" placeholder="Primeiro nome">
                                         </div>
                                         <div class="col-md-6">
-                                            <label>Last Name"</label>
-                                            <input class="form-control" type="text" placeholder="Last Name">
+                                            <label>Sobrenome</label>
+                                            <input class="form-control" type="text" placeholder="Sobrenome">
                                         </div>
                                         <div class="col-md-6">
                                             <label>E-mail</label>
                                             <input class="form-control" type="text" placeholder="E-mail">
                                         </div>
                                         <div class="col-md-6">
-                                            <label>Mobile No</label>
-                                            <input class="form-control" type="text" placeholder="Mobile No">
+                                            <label>Número de telefone</label>
+                                            <input class="form-control" type="text" placeholder="Número de telefone">
                                         </div>
                                         <div class="col-md-12">
-                                            <label>Address</label>
-                                            <input class="form-control" type="text" placeholder="Address">
-                                        </div>
+                                            <label>Endereço</label>
+                                            <input class="form-control" type="text" placeholder="Endereço">
+                                        </div>                                   
                                         <div class="col-md-6">
-                                            <label>Country</label>
-                                            <select class="custom-select">
-                                                <option selected>United States</option>
-                                                <option>Afghanistan</option>
-                                                <option>Albania</option>
-                                                <option>Algeria</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label>City</label>
-                                            <input class="form-control" type="text" placeholder="City">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label>State</label>
-                                            <input class="form-control" type="text" placeholder="State">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label>ZIP Code</label>
-                                            <input class="form-control" type="text" placeholder="ZIP Code">
+                                            <label>CEP</label>
+                                            <input class="form-control" type="text" placeholder="CEP">
                                         </div>
                                         <div class="col-md-12">
                                             <div class="custom-control custom-checkbox">
                                                 <input type="checkbox" class="custom-control-input" id="newaccount">
-                                                <label class="custom-control-label" for="newaccount">Create an account</label>
+                                                <label class="custom-control-label" for="newaccount">Criar Conta</label>
                                             </div>
                                         </div>
                                         <div class="col-md-12">
                                             <div class="custom-control custom-checkbox">
                                                 <input type="checkbox" class="custom-control-input" id="shipto">
-                                                <label class="custom-control-label" for="shipto">Ship to different address</label>
+                                                <label class="custom-control-label" for="shipto">Enviar para outro endereço</label>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div class="shipping-address">
-                                    <h2>Shipping Address</h2>
+                                    <h2>Endereço para envio</h2>
                                     <div class="row">
                                         <div class="col-md-6">
-                                            <label>First Name</label>
-                                            <input class="form-control" type="text" placeholder="First Name">
+                                            <label>Primeiro nome</label>
+                                            <input class="form-control" type="text" placeholder="Primeiro nome">
                                         </div>
                                         <div class="col-md-6">
-                                            <label>Last Name"</label>
-                                            <input class="form-control" type="text" placeholder="Last Name">
+                                            <label>Sobrenome</label>
+                                            <input class="form-control" type="text" placeholder="Sobrenome">
                                         </div>
                                         <div class="col-md-6">
                                             <label>E-mail</label>
                                             <input class="form-control" type="text" placeholder="E-mail">
                                         </div>
                                         <div class="col-md-6">
-                                            <label>Mobile No</label>
-                                            <input class="form-control" type="text" placeholder="Mobile No">
+                                            <label>Número de telefone</label>
+                                            <input class="form-control" type="text" placeholder="Número de telefone">
                                         </div>
                                         <div class="col-md-12">
-                                            <label>Address</label>
-                                            <input class="form-control" type="text" placeholder="Address">
+                                            <label>Endereço</label>
+                                            <input class="form-control" type="text" placeholder="Endereço">
                                         </div>
                                         <div class="col-md-6">
-                                            <label>Country</label>
-                                            <select class="custom-select">
-                                                <option selected>United States</option>
-                                                <option>Afghanistan</option>
-                                                <option>Albania</option>
-                                                <option>Algeria</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label>City</label>
-                                            <input class="form-control" type="text" placeholder="City">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label>State</label>
-                                            <input class="form-control" type="text" placeholder="State">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label>ZIP Code</label>
-                                            <input class="form-control" type="text" placeholder="ZIP Code">
+                                            <label>CEP</label>
+                                            <input class="form-control" type="text" placeholder="CEP">
                                         </div>
                                     </div>
                                 </div>
@@ -129,20 +95,20 @@
                         <div class="col-lg-4">
                             <div class="checkout-inner">
                                 <div class="checkout-summary">
-                                    <h1>Cart Total</h1>
-                                    <p>Product Name<span>$99</span></p>
-                                    <p class="sub-total">Sub Total<span>$99</span></p>
-                                    <p class="ship-cost">Shipping Cost<span>$1</span></p>
-                                    <h2>Grand Total<span>$100</span></h2>
+                                    <h1>Total do carrinho</h1>
+                                    <p>Nome do Produto<span>R$99</span></p>
+                                    <p class="sub-total">Subtotal<span>R$99</span></p>
+                                    <p class="ship-cost">Frete<span>R$1</span></p>
+                                    <h2>Total geral<span>R$100</span></h2>
                                 </div>
 
                                 <div class="checkout-payment">
                                     <div class="payment-methods">
-                                        <h1>Payment Methods</h1>
+                                        <h1>Métodos de Pagamento</h1>
                                         <div class="payment-method">
                                             <div class="custom-control custom-radio">
                                                 <input type="radio" class="custom-control-input" id="payment-1" name="payment">
-                                                <label class="custom-control-label" for="payment-1">Paypal</label>
+                                                <label class="custom-control-label" for="payment-1">Cartão de crédito</label>
                                             </div>
                                             <div class="payment-content" id="payment-1-show">
                                                 <p>
@@ -152,19 +118,8 @@
                                         </div>
                                         <div class="payment-method">
                                             <div class="custom-control custom-radio">
-                                                <input type="radio" class="custom-control-input" id="payment-2" name="payment">
-                                                <label class="custom-control-label" for="payment-2">Payoneer</label>
-                                            </div>
-                                            <div class="payment-content" id="payment-2-show">
-                                                <p>
-                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tincidunt orci ac eros volutpat maximus lacinia quis diam.
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div class="payment-method">
-                                            <div class="custom-control custom-radio">
                                                 <input type="radio" class="custom-control-input" id="payment-3" name="payment">
-                                                <label class="custom-control-label" for="payment-3">Check Payment</label>
+                                                <label class="custom-control-label" for="payment-3">Cheque de pagamento</label>
                                             </div>
                                             <div class="payment-content" id="payment-3-show">
                                                 <p>
@@ -175,7 +130,7 @@
                                         <div class="payment-method">
                                             <div class="custom-control custom-radio">
                                                 <input type="radio" class="custom-control-input" id="payment-4" name="payment">
-                                                <label class="custom-control-label" for="payment-4">Direct Bank Transfer</label>
+                                                <label class="custom-control-label" for="payment-4">Transferência bancária direta</label>
                                             </div>
                                             <div class="payment-content" id="payment-4-show">
                                                 <p>
@@ -186,7 +141,7 @@
                                         <div class="payment-method">
                                             <div class="custom-control custom-radio">
                                                 <input type="radio" class="custom-control-input" id="payment-5" name="payment">
-                                                <label class="custom-control-label" for="payment-5">Cash on Delivery</label>
+                                                <label class="custom-control-label" for="payment-5">Dinheiro na entrega</label>
                                             </div>
                                             <div class="payment-content" id="payment-5-show">
                                                 <p>
@@ -196,7 +151,7 @@
                                         </div>
                                     </div>
                                     <div class="checkout-btn">
-                                        <button>Place Order</button>
+                                        <button>Faça a encomenda</button>
                                     </div>
                                 </div>
                             </div>

5c0bd8a8eacd4b70ea1a7d77db85a3fabf5c6727
Ricardo Valente <51724489+RiiKi-Kenway@users.noreply.github.com>
2021-06-16 19:50:54 -0300

    correção register

--- Controller/Register.php
+++ Controller/Register.php
@@ -1,6 +1,6 @@
 <?php
 require "Model/Usuarios.php";
-class ControllerRegister
+class Register
 {
 
     private $user;
@@ -24,24 +24,7 @@ class ControllerRegister
         $this->user->setN_Casa($_POST["n_Casa"]);
         $this->user->setComplemento($_POST["complemento"]);
 
-        if ($this->user->addUsuarios()) {
-            // echo "<script>alert('SALVEI SEUS DADOS  !'); </script>";
-            header('Location:Home.php', true, 302);
-        }
-        // Deu ruim
-        header('Location:404.php', true, 302);
-    }
-
-    public function ajax()
-    {
-        $cep = $_POST['cep'];
-
-        $reg = simplexml_load_file("http://cep.republicavirtual.com.br/web_cep.php?formato=xml&cep=" . $cep);
-
-        $dados['sucesso'] = (string) $reg->resultado;
-        $dados['rua']     = (string) $reg->tipo_logradouro . ' ' . $reg->logradouro;
-        $dados['bairro']  = (string) $reg->bairro;
-
-        echo json_encode($dados);
+        $this->user->addUsuarios();
+        header('Location:Home.php', true, 302);
     }
 }
